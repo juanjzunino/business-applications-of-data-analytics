ff0cf8967b89d82143acd975e133fdb5e0d1c407
Juanjo Zunino <juanj.zunino@gmail.com>
2020-09-23 22:15:41 -0300

    Matching on Education

---
+++ functions.R
@@ -0,0 +1,29 @@
+smahal <- function(z,X) {
+    X<-as.matrix(X)
+    n<-dim(X)[1]
+    rownames(X)<-1:n
+    k<-dim(X)[2]
+    m<-sum(z)
+    for (j in 1:k) X[,j]<-rank(X[,j])
+    cv<-cov(X)
+    vuntied<-var(1:n)
+    rat<-sqrt(vuntied/diag(cv))
+    cv<-diag(rat)%*%cv%*%diag(rat)
+    out<-matrix(NA,m,n-m)
+    Xc<-X[z==0,]
+    Xt<-X[z==1,]
+    rownames(out)<-rownames(X)[z==1]
+    colnames(out)<-rownames(X)[z==0]
+    library(MASS)
+    icov<-ginv(cv)
+    for (i in 1:m) out[i,]<-mahalanobis(Xc,Xt[i,],icov,inverted=T)
+    out
+  }
+
+addcaliper <- function(dmat,z,logitp,calipersd=.2,penalty=1000) {
+  sd.logitp=sd(logitp)
+  adif=abs(outer(logitp[z==1],logitp[z==0],"-"))
+  adif=(adif-(calipersd*sd.logitp))*(adif>(calipersd*sd.logitp))
+  dmat=dmat+adif*penalty
+  dmat
+}
---
+++ matching.R
@@ -0,0 +1,239 @@
+"
+                          Business Application of Data Analytics      
+                                
+                              Professor: Santiago Gallino               
+                               Student: Juan José Zunino                 
+                                             
+"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                     CONFIGURATION                                      #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#############################
+#        Environment        #
+#############################
+
+# Setup environment
+rm(list=ls())
+setwd('/Users/juanjozunino/Documents/GitHub/business-applications-of-data-analytics/')
+
+# Import libraries
+library(dplyr)
+library(ggplot2)
+library(coin)
+library(optmatch)
+library(exactRankTests)
+library(MASS)
+library(gap)
+
+
+
+#############################
+#           Data            #
+#############################
+
+# Load data
+data <- read.csv('dataset.csv', header = TRUE, na.strings = c(""))
+
+# Load functions
+source('functions.R')
+
+# Parse data
+dim(data)
+summary(data) # Gender, Marrried and Self_Employed presents missing data
+
+data$Loan_Status <- ifelse(data$Loan_Status == 'Y', 1, 0)
+data$Dependents <- as.factor(ifelse(data$Dependents == 0, 0, 1))
+data$Credit_History <- as.factor(data$Credit_History)
+data$Loan_ID <- NULL
+
+
+# Remove rows with missing data
+table(complete.cases(data)) # 480 complete cases
+data <- data[complete.cases(data),]
+summary(data)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                 DESCRIPTIVE STATISTICS                                 #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# Proportions
+table(data$Education)
+
+round(prop.table(table(data$Loan_Status[data$Education == 'Graduate'])), 2)
+round(prop.table(table(data$Loan_Status[data$Education == 'Not Graduate'])), 2)
+
+# t-test
+treated.y=data$Loan_Status[data$Education == 'Graduate'];
+control.y=data$Loan_Status[data$Education == 'Not Graduate'];
+
+t.test(treated.y, control.y)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                             MATCHING w/ PROPENSITY SCORE                               #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#############################
+#     Propensity score      #
+#############################
+
+# Model
+propscore.model <- glm(Education ~ .,
+                        family = binomial,
+                        x = TRUE,
+                        y = TRUE,
+                        data = data[,setdiff(colnames(data), "Loan_Status")])
+summary(propscore.model)
+
+# Set treatment variable
+treated <- propscore.model$y
+
+# Find logit(propensity score)
+logit.propscore <- predict(propscore.model)
+
+
+#############################
+#      Distance matrix      #
+#############################
+
+# Construct a distance matrix which gives the absolute 
+# difference between the propensity scores 
+# of the treated and control subjects
+
+distmat.propensity=matrix(rep(0,sum(treated==1)*sum(treated==0)),nrow=sum(treated==1))
+
+for(i in 1:sum(treated==1)){
+  for(j in 1:sum(treated==0)){
+    distmat.propensity[i,j]=abs((logit.propscore[treated==1])[i]-(logit.propscore[treated==0])[j])
+  }
+}
+
+
+
+# Create a subject index and name the rows and columns of distance
+# matrix by ### this subject index
+subject.index=seq(1,length(treated),1)
+rownames(distmat.propensity)=subject.index[treated==1]
+colnames(distmat.propensity)=subject.index[treated==0]
+
+
+#############################
+#       Pair matching       #
+#############################
+
+matchvec <- pairmatch(distmat.propensity)
+
+
+treated.subject.index=rep(0,sum(treated==1))
+matched.control.subject.index=rep(0,length(treated.subject.index))
+matchedset.index=substr(matchvec,start=3,stop=10)
+matchedset.index.numeric=as.numeric(matchedset.index)
+subjects.match.order=as.numeric(names(matchvec))
+
+# The subject indices in 
+# the order of matchvec
+for(i in 1:length(treated.subject.index)){
+  matched.set.temp=which(matchedset.index.numeric==i)
+  matched.set.temp.indices=subjects.match.order[matched.set.temp]
+  if(treated[matched.set.temp.indices[1]]==1){
+    treated.subject.index[i]=matched.set.temp.indices[1]
+    matched.control.subject.index[i]=matched.set.temp.indices[2]
+  }
+  if(treated[matched.set.temp.indices[2]]==1) {
+    treated.subject.index[i]=matched.set.temp.indices[2]
+    matched.control.subject.index[i]=matched.set.temp.indices[1]
+  }
+}
+
+# Calculate standardized differences 
+# Covariates used in propensity score model
+Xmat=propscore.model$x;
+treatedmat=Xmat[treated==1,];
+# Standardized differences before matching
+controlmat.before=Xmat[treated==0,];
+controlmean.before=apply(controlmat.before,2,mean);
+treatmean=apply(treatedmat,2,mean);
+treatvar=apply(treatedmat,2,var);
+controlvar=apply(controlmat.before,2,var);
+stand.diff.before=(treatmean-controlmean.before)/sqrt((treatvar+controlvar)/2);
+
+# Standardized differences after matching
+controlmat.after=Xmat[matched.control.subject.index,];
+controlmean.after=apply(controlmat.after,2,mean);
+# Standardized differences after matching
+stand.diff.after=(treatmean-controlmean.after)/sqrt((treatvar+controlvar)/2);
+cbind(stand.diff.before,stand.diff.after)
+
+
+# Before
+pval.t.test.b=rep(0,ncol(Xmat)-1);
+varnames=(dimnames(Xmat)[[2]])[2:ncol(Xmat)];
+for(j in 2:ncol(Xmat)){
+  pval.t.test.b[j-1]=t.test(treatedmat[,j],controlmat.before[,j])$p.val;
+}
+
+cbind(varnames,pval.t.test.b);
+
+# After
+pval.t.test=rep(0,ncol(Xmat)-1);
+varnames=(dimnames(Xmat)[[2]])[2:ncol(Xmat)];
+for(j in 2:ncol(Xmat)){
+  pval.t.test[j-1]=t.test(treatedmat[,j],controlmat.after[,j])$p.val;
+}
+
+cbind(varnames,pval.t.test);
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                 MATCHING w/ CALIPER                                    #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+treated <- propscore.model$y
+
+# Matrix of covariates, excluding intercept
+Xmat=propscore.model$x[,-1]
+# Rank based Mahalanobis distance
+distmat=smahal(treated,Xmat)
+# Add caliper
+logit.propscore=predict(propscore.model)
+distmat2=addcaliper(distmat,treated,logit.propscore)
+
+### Create a subject index and name the rows and columns
+#of distance matrix by ### this subject index
+
+subject.index=seq(1,length(treated),1)
+rownames(distmat2)=subject.index[treated==1]
+colnames(distmat2)=subject.index[treated==0]
+
+# Pair Matching
+matchvec=pairmatch(distmat2)
+
+# Note: Can ignore warning message from matching
+
+# Create vectors of the subject indices of the treatment
+# units ordered by
+# their matched set and corresponding control unit
+
+treated.subject.index=rep(0,sum(treated==1))
+matched.control.subject.index=rep(0,length(treated.subject.index))
+matchedset.index=substr(matchvec,start=3,stop=10)
+matchedset.index.numeric=as.numeric(matchedset.index)
+subjects.match.order=as.numeric(names(matchvec)) # The subject indices in 
+
+# the order of matchvec
+for(i in 1:length(treated.subject.index)){
+  matched.set.temp=which(matchedset.index.numeric==i)
+  matched.set.temp.indices=subjects.match.order[matched.set.temp]
+  if(treated[matched.set.temp.indices[1]]==1){
+    treated.subject.index[i]=matched.set.temp.indices[1]
+    matched.control.subject.index[i]=matched.set.temp.indices[2]
+  }
+  if(treated[matched.set.temp.indices[2]]==1){
+    treated.subject.index[i]=matched.set.temp.indices[2]
+    matched.control.subject.index[i]=matched.set.temp.indices[1]
+  }
+}
